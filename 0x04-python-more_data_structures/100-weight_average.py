22307ce7723d1336031958253d28d1a35705d4f1
oluwagbayilo <princegeeby1@gmail.com>
2024-01-12 23:19:48 +0100

    Add new solution to task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    if not my_list or not all(isinstance(tup, tuple) and len(tup) == 2 for tup in my_list):
+    if not my_list:
         return 0
 
-    total_weighted_sum = sum(weight * value for weight, value in my_list)
-    total_weights = sum(weight for weight, _ in my_list)
+    weighted_sum= 0
+    total_weights = 0
 
-    if total_weights == 0:
-        return 0
+    for tup in my_list:
+        weighted_sum += tup[0] * tup[1]
+        total_weights += tup[1]
 
-    return total_weighted_sum / total_weights
+    return weighted_sum / total_weights
