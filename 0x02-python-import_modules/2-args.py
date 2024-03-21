278e85ce08635916633d46455b10fd8ce5837cfe
oluwagbayilo <princegeeby1@gmail.com>
2024-03-21 15:00:23 +0100

    Add new solution to task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+if __name__ == '__main__':
+    from sys import argv
+    
+    if len(argv) - 1 == 0:
+        print('0 arguments.')
+    elif len(argv) - 1 == 1:
+        print('1 argument:')
+        print('1:', argv[1])
     else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print(len(argv) - 1, 'arguments:')
+        count = 1
+        while count < len(argv):
+            print('{}: {}'.format(count, argv[count]))
+            count += 1
