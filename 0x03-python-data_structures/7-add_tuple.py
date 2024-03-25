7068080a61b6a12a1c1ef78a2546f60089aeb484
oluwagbayilo <princegeeby1@gmail.com>
2024-03-25 12:10:12 +0100

    Fix error in solution

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,6 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) == 1:
         (a,) = tuple_a
         b = 0
+    elif len(tuple_a) > 2:
+        a, b = tuple_a[0:2]
     else:
         a, b = tuple_a
 
@@ -14,6 +16,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) == 1:
         (c,) = tuple_b
         d = 0
+    elif len(tuple_b) > 2:
+        c, d = tuple_b[0:2]
     else:
         c, d = tuple_b
 
