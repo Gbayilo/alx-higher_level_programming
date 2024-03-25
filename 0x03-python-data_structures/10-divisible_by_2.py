ffe79a79e005ed9ec372e232e2785f9f0de90944
oluwagbayilo <princegeeby1@gmail.com>
2024-03-25 19:15:17 +0100

    Add new solution to task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    return [True if x % 2 == 0 else False for x in my_list]
+    if not my_list:
+        return
+    new_list = [num % 2 == 0 for num in my_list]
+    return (new_list)
