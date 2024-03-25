1cefc34f024e85f5aff75b28cd816690bd9e5f4a
oluwagbayilo <princegeeby1@gmail.com>
2024-03-25 18:48:17 +0100

    Add new solution to task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    length = len(my_list)
-
-    if length == 0:
-        return (None)
-
-    max_int = my_list[0]
-
-    for i in range(1, length):
-        if my_list[i] > max_int:
-            max_int = my_list[i]
-
-    return (max_int)
+    if my_list == []:
+        return
+    while len(my_list) > 1:
+        my_list = [
+                my_list[i]
+                for i in range(len(my_list) - 1)
+                if my_list[i] >= my_list[i + 1]
+                ]
+    return my_list[0]
