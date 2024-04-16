58d97b9060887d1432dbc178b74f84ac11fb3345
oluwagbayilo <princegeeby1@gmail.com>
2024-04-16 12:25:40 +0100

    Add solution to task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file using UTF8 encoding
+    and returns the number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
