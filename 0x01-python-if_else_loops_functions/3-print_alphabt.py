ef0775b9dbd7dd5189cb6cdddaa41748b4501884
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2026-01-17 10:50:31 +0100

    Task 0x01(3): Add alternate solution to print ASCII alphabet except 'q' and 'e'

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i not in [101, 113]:
-        print("{:c}".format(i), end="")
+
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('e') and i != ord('q'):  
+        print(chr(i), end='')
